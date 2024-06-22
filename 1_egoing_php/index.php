60bc425a4f7a21a4edcbaae266056bdaedb89cd7
kyumin1227 <kyumin12271227@gmail.com>
2024-06-22 16:25:04 +0900

    Study: First PHP
    - 생활코딩 PHP ( ~ PHP의 URL 파라미터)

---
+++ 1_egoing_php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+  <body>
+  <h1>WEB</h1>
+    <ol>
+        <li><a href="index.php?id=HTML">HTML</a></li>
+        <li><a href="index.php?id=CSS">CSS</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+  </body>
+</html>
