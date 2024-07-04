5f7899e628ce00cbae52c8e1f0e76f404657c692
kyumin1227 <kyumin12271227@gmail.com>
2024-07-04 20:59:52 +0900

    Study: PHP (생활코딩)
    - 생활코딩 PHP ( ~ PHP에서 글수정 기능 구현하기)

--- 1_egoing_php/create.php
+++ 1_egoing_php/create.php
@@ -40,16 +40,17 @@
         ?>
     </ol>
   <a href="create.php">create</a>
-  <?php if (isset($_GET["id"])) { ?>
-      <a href="update.php?id=<?php echo $_GET["id"]; ?> ">update</a>
-  <?php } ?>
-  <h2>
-      <?php
-        print_title();
-      ?>
-  </h2>
-  <?php
-    print_description();
-  ?>
+  <form action="create_process.php" method="post">
+      <p>
+          <input type="text" name="title" placeholder="Title">
+      </p>
+      <p>
+          <textarea name="description" placeholder="Description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+  </form>
+
   </body>
 </html>
--- 1_egoing_php/update.php
+++ 1_egoing_php/update.php
@@ -44,6 +44,18 @@
       <a href="update.php?id=<?php echo $_GET["id"]; ?> ">update</a>
   <?php } ?>
   <h2>
+      <form action="update_process.php" method="post">
+          <input type="hidden" name="old_title" value="<?= $_GET["id"] ?>">
+          <p>
+              <input type="text" name="title" placeholder="Title" value="<?php print_title(); ?>">
+          </p>
+          <p>
+              <textarea name="description" placeholder="Description"><?php print_description(); ?></textarea>
+          </p>
+          <p>
+              <input type="submit">
+          </p>
+      </form>
       <?php
         print_title();
       ?>
