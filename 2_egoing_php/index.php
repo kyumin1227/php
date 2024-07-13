0f32a028011d4080c6b70a39a84d5fc8a4ead0a7
kyumin1227 <kyumin12271227@gmail.com>
2024-07-13 21:52:34 +0900

    Study: PHP & MySQL (생활코딩)
    - 생활코딩 PHP & MySQL ( ~ 글생성 (CREATE))

--- 2_egoing_php/index.php
+++ 2_egoing_php/index.php
@@ -9,6 +9,7 @@
 <ol>
     <li>HTML</li>
 </ol>
+<a href="create.php">create</a>
 <h2>Welcome</h2>
 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis corporis, doloremque ducimus illum, ipsam iste magnam magni neque placeat quaerat quam qui ut. Aperiam fuga laboriosam nam omnis, praesentium vitae.
 </body>
